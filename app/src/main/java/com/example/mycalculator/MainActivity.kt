8bb065f95578fa02a13cd6e724d27c1ec9cb2b9a
Vishesh Jain <vishesh20550@iiitd.ac.in>
2022-08-11 13:28:50 +0530

    All done

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.mycalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import java.lang.ArithmeticException
 import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity() {
@@ -41,10 +43,9 @@ class MainActivity : AppCompatActivity() {
         isDecimalActive=false;
     }
     fun onOperatorClicked(view : View){
-
+        val operator = (view as Button).text
         if(!isOperatorActive){
-            val operator = (view as Button).text
-            if(textView?.text==null && textView?.text!!.isEmpty()){
+            if(textView?.text==null || textView?.text == ""){
                 if(operator== "-"){
                     textView?.append("(-)");
                 }
@@ -66,7 +67,7 @@ class MainActivity : AppCompatActivity() {
             }
             isDecimalActive = false
         }else{
-            if((view as Button).text.equals("-")){
+            if(operator.equals("-")){
                 if (textView?.text?.get(textView?.text!!.length - 1)?.equals('+') == true) {
                     onDelClicked(view)
                     textView?.append("-");
@@ -78,7 +79,7 @@ class MainActivity : AppCompatActivity() {
                 }else if(textView?.text?.get(textView?.text!!.length - 1)?.equals('*') == true || textView?.text?.get(textView?.text!!.length - 1)?.equals('/') == true){
                     textView?.append("(-)")
                 }
-            }else if((view as Button).text.equals("+")){
+            }else if(operator.equals("+")){
                 if (textView?.text?.get(textView?.text!!.length - 1)?.equals('-') == true) {
                     onDelClicked(view)
                     textView?.append("-");
@@ -88,11 +89,89 @@ class MainActivity : AppCompatActivity() {
         }
     }
     private fun printOutput(){
-        val str = textView?.text;
-        if(str?.contains('+') == false && !str.contains('-') && !str.contains('*') && !str.contains('/')){
+        val str = textView?.text.toString();
+        if(!str.contains('+') && !str.contains('-') && !str.contains('*') && !str.contains('/')){
             return;
         }else{
+            try {
+                var isNegative = false;
+                var start = 0;
+                var num1 = ""
+                var num2 = ""
+                var operatorActive = '0'
+                var i =0
+                while(i < str.length){
+                    if(str[i] == '('){
+                        isNegative = true
+                        i+=3;
+                        start=i
+                    }
 
+                    if(str[i] == '+' || str[i] == '*' ||str[i] == '/' ||str[i] == '-' || i==str.length-1){
+                       if(num1 == ""){
+                           num1= str.substring(start,i);
+                           if(isNegative){
+                               num1= "-$num1";
+                               isNegative=false;
+                           }
+                           num2=""
+                           start = i+1;
+                           operatorActive = str[i]
+                       }else if(num2 == ""){
+                           if(i==str.length-1){
+                               i++;
+                           }
+                           num2 = str.substring(start,i)
+                           if(isNegative){
+                               num2= "-$num2";
+                               isNegative=false;
+                           }
+                           start = i+1;
+                           var num1Double = num1.toDouble();
+                           val num2Double = num2.toDouble();
+                           if(operatorActive == '+'){
+                               num1Double += num2Double
+                           }else if(operatorActive == '-'){
+                               num1Double -= num2Double
+                           }else if(operatorActive == '*'){
+                               num1Double *= num2Double
+                           }else if(operatorActive == '/'){
+                               num1Double /= num2Double
+                               if(num1Double== Double.POSITIVE_INFINITY || num1Double== Double.NEGATIVE_INFINITY){
+                                   Toast.makeText(this, "Infinity >>>>>",Toast.LENGTH_SHORT).show();
+                                   textView?.text =""
+                                   isDecimalActive=true;
+                                   isOperatorActive=false;
+                                   return;
+                               }
+                           }
+                           num1 = num1Double.toString()
+                           num2=""
+                           if(i<str.length-1){
+                               operatorActive = str[i]
+                           }
+                           if(i>=str.length){
+                               var strTemp = num1Double.toString();
+                               if(num1Double<0){
+                                   strTemp = "(-)${strTemp.substring(1)}"
+                               }
+                               textView?.text = strTemp;
+                               isDecimalActive=true;
+                               isOperatorActive=false;
+                           }
+                       }
+                    }
+                    i++;
+                }
+            }catch (e  : ArithmeticException){
+                Toast.makeText(this, "Wrong Calculation parameters (eg. divide by zero)",Toast.LENGTH_SHORT).show();
+                textView?.text =""
+                isDecimalActive=true;
+                isOperatorActive=false;
+                e.printStackTrace();
+                return
+//                Log.i("Arithmetic Error",e.toString());
+            }
         }
     }
     fun onDelClicked(view : View){
